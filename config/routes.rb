e9b55037e1bc8868f2e2d8b16b872c204154bdf2
ryanmmhmm <magowan.ryan@gmail.com>
2015-12-13 19:03:35 -0500

    changing resources to user_sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :userSessions, only: [:new, :create, :destroy]
+  resources :user_sessions, only: [:new, :create, :destroy]
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
