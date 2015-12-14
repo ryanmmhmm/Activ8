84ab091878a219d484f69a6d34c8b222066a7bba
ryanmmhmm <magowan.ryan@gmail.com>
2015-12-14 00:36:07 -0500

    making the activities controller full of fresh code for index, show, new, create, edit, update, destroy and private:strong_params

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,19 +1,66 @@
 class ActivitiesController < ApplicationController
+  skip_before_action :require_login, only: [:index, :show]
+
   def index
+    @activities = Activity.all
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
   end
 
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(activity_params)
+
+    if @activity.save
+      redirect_to @activity, notice: "#{@activity.title.capitalize} has been Acitv8'd."
+    else
+      render :new, alert: 'Making your activity has wuba-duba-dub-dubbed... Call Rick.'
+    end
+
   end
 
   def edit
+    @activity = Activity.find(params[:id])
   end
 
   def update
+    @activity = Activity.find(params[:id])
+
+    if @activity.update_attributes(activities_params)
+      redirect_to @activity, notice: "#{@activity.title.capitalize} was updated."
+    else
+      render :edit
+    end
+
   end
 
   def destroy
+    if @activity.destroy
+      redicret_to :root, notice: 'Your account has be deActiv8\'d'
+    elsif @activity.errors.is_true?
+      flash.now[:alert] = 'Sorry, you can\'t deActiv8 that.'
+      redirect_back_or_to (:activities)
+    else
+      logout(current_user)
+      redirect_to :root, notice: 'Get out, heathen!'
+    end
+
+  end
+
+  private
+  def activities_params
+    require(:activities).permit(
+    params[:id,
+      :title,
+      :description,
+      :location,
+      :end_time,
+      :picture_url,
+      :owner_id])
   end
 end
