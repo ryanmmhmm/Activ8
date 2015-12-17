7e5069e9c07053b3dd587273c1958f8f154aa019
ryanmmhmm <magowan.ryan@gmail.com>
2015-12-17 13:53:16 -0500

    filling out index, new, create, edit, update and comment_params methods.  Untested in rails c yet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,53 @@
 class CommentsController < ApplicationController
+  skip_before_action :require_login, only: [:index, :show]
+
   def index
+    @comments = Comment.all
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.activity = comment_params[:activity_id]
+
+    if @comment.save
+      redirect_to activity_path(@comment.activity)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(comment_params[:id])
+
+    if (@comment.user == current_user) && (@comment.update(comment_params))
+      redirect_to activity_path(@comment.activity)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(
+      :id,
+      :user_id,
+      :activity_id,
+      :body)
   end
 end
