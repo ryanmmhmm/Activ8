4bc826cb89b33176d581191afb9f0a14632250b8
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-12-18 16:44:18 -0500

    added javascript that returns lat and lon of users current position

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -6,10 +6,21 @@ $(document).on("ready page:load", function(){
   })
 })
 
-function success() {
-  console.log('it workd!');
+function success(position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  console.log(lat, lon);
+  $.ajax({
+    url: '/activities',
+    method: 'get',
+    data: {'latitude': lat, 'longitude': lon},
+    dataType: 'html',
+    success: function(data) {
+      console.log('it worked!' + data);
+    }
+  })
 }
 
-function error() {
-  console.log('something went wrong');
+function error(err) {
+  console.log('something went wrong' + err.message);
 }
