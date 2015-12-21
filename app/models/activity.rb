bfc83ee09055fad0fa0b8ea32bdd8a34fa1e1b14
ryanmmhmm <magowan.ryan@gmail.com>
2015-12-20 22:35:00 -0500

    validaing owner

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,7 @@ class Activity < ActiveRecord::Base
   belongs_to :owner, class_name: User
   has_many :comments
 
+  validates :owner_id, presence: true
   validates :title, presence: true
   validates :end_time, presence: true
 end
