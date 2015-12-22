66907e3936291e83306ded70cd2c5bea281e466f
ryanmmhmm <magowan.ryan@gmail.com>
2015-12-22 17:04:08 -0500

    adding new seed data for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,42 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 ## Create Users
-User.create(email: "test@test.com", password: "12345Test", password_confirmation: "12345Test", username: "anon", picture_url: "http://www.torontomike.com/images/blog/starwars.gif")
-User.create(email: 'no_activity', password: 'Temp123!', password_confirmation: 'Temp123!', username: 'no_activity', picture_url: 'http://img2.wikia.nocookie.net/__cb20120417200552/beyblade/images/2/21/Forever_alone_icon_by_projectendo-d2zlt6v.jpg')
-User.create(email: "asdf", password: "asdfg", password_confirmation: "asdfg", username: "anonimus", picture_url: "https://pbs.twimg.com/profile_images/1125323984/darthvader_normal.jpg")
+User.create(email: "user1@app.com", password: "12345Test", password_confirmation: "12345Test", username: "anonymous", picture_url: "http://www.torontomike.com/images/blog/starwars.gif")
+User.create(email: 'no_activity@app.com', password: 'Temp123!', password_confirmation: 'Temp123!', username: 'no_activity', picture_url: 'http://img2-ak.lst.fm/i/u/64s/2291dfce5f5c4a05adb2f59dc597d0f5.jpg')
+User.create(email: "user2@app.com", password: "asdfg", password_confirmation: "asdfg", username: "anonimus", picture_url: "http://static.stereoboard.com/images/artistimages/50/50x50xtn_james-blake.jpg.pagespeed.ic.o2lHPXbI9M.jpg")
 
-@activity = Activity.create(title: "Custom Fanstasy Trading Application Day", description: "Matt, Andrew, Tyler & Alon present their amazing project where you can plan fantasy, and make stock trades, and do them both at the same time, wubadubadubdub.", end_time: Date.tomorrow, location: "220 King St W, Suite 200, Toronto, ON M5H 1K4", picture_url: "http://hd-report.com/wp-content/uploads/2014/09/espn-fantasy-football-app-logo-sq.jpg")
+@activity = Activity.create(title: "Bitmaker Demo-day NOW!", description: "Lets go Team Gibson.", end_time: Date.today, location: "220 King St W, Suite 200, Toronto, ON M5H 1K4", picture_url: "http://blog.bitmakerlabs.com/content/images/2015/09/blog-logo.png")
 @activity.owner = User.first
 @activity.save
-@comment = @activity.comments.create(body: "I'm going to win this league of Traders.")
+@comment = @activity.comments.create(body: "Andrew sucks at Chess.")
 @comment.user = User.last
 @comment.save
 
-@activity = Activity.create(title: "Star Wars Movie Premiere: Always Tomorrow", description: "Dew, dew dew dew dew DEWWWWW DEW dew dew dew DEWWWWWW DEW dew dew dew DEWWWW!", end_time: Date.tomorrow, location: "259 Richmond St W, Toronto, ON M5V 3M6", picture_url: "http://pixel.nymag.com/imgs/daily/vulture/2015/10/18/18-star-wars-poster.w529.h529.jpg")
+@activity = Activity.create(title: "Star Wars Movie Premiere: Always Tomorrow", description: "Jar Jar Binks is a Sith Lord.", end_time: Date.tomorrow, location: "259 Richmond St W, Toronto, ON M5V 3M6", picture_url: "http://pixel.nymag.com/imgs/daily/vulture/2015/10/18/18-star-wars-poster.w529.h529.jpg")
 @activity.owner = User.last
 @activity.save
 @comment = @activity.comments.create(body: "I can't wait to see this movie.")
 @comment.user = User.first
 @comment.save
 
+@activity = Activity.create(title: "Watch the Jays lose in the playoffs :(", description: "Joey Bat Flip.", end_time: Date.tomorrow, location: "1 Blue Jays Way, Toronto, ON M5V 1J1", picture_url: "http://www.blogto.com/upload/2011/11/20111119-blue-jays-better.jpg")
+@activity.owner = User.last
+@activity.save
+
+@comment = @activity.comments.create(body: "Take me out to the ball game,
+Take me out with the crowd;
+Just buy me some peanuts and Cracker Jack,
+I don't care if I never get back.
+Let me root, root, root for the home team,
+If they don't win, it's a shame.
+For it's one, two, three strikes, you're out,
+At the old ball game.")
+@comment.user = User.last
+@comment.save
+
+@activity = Activity.create(title: "New Battletoads just released.", description: "Call: (416) 861-9521 to pre-order your copy.", end_time: Date.tomorrow, location: "199 Bay St C-134, Toronto, ON M5L 1E7", picture_url: "https://upload.wikimedia.org/wikipedia/en/d/df/Battletoads_in_Battlemaniacs.png")
+@activity.owner = User.first
+@activity.save
+@acomment = @activity.comments.create(body: "Woah, I never thought this was going to be released!")
+@comment.user = User.last
+@comment.save
