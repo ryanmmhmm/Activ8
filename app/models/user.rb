0bd2c85ed5fff8c75688c3d051af836eee310c57
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-05 01:30:22 -0500

    when Owner deletes an activity, all comments on that activity are destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :owned_activities, class_name: 'Activity', foreign_key: 'owner_id'
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
+  # Sorcery related
   validates :password, length: { minimum: 5}, if: -> { new_record? || changes["password"] }
   validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
 
+  #Custom validations
   validates :email, uniqueness: true
 end
