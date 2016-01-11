8805843d6955581b0ee035affd9f4745a4267fb8
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-11 16:17:19 -0500

    changing location information for activities, shortening comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,6 @@ User.create(email: 'no_activity@app.com', password: 'Temp123!', password_confirm
 User.create(email: "contact@robholody.io", password: "asdfg", password_confirmation: "asdfg", username: "Rob", picture_url: "http://robholody.io/img/rob-head.png")
 User.create(email: "scmwalsh@gmail.com", password: "larry", password_confirmation: "larry", username: "SCMWalsh", picture_url: "https://scontent-yyz1-1.xx.fbcdn.net/hphotos-frc3/v/t1.0-9/1978760_10153857569815008_1296979771_n.jpg?oh=83541e7d0a4bca3f4672ba08ec7f4b7a&oe=56FD106D")
 
-@activity = Activity.create(title: "Bitmaker Demo-day NOW!", description: "Lets go Team Gibson.", end_time: DateTime.now, location_title: "Bitmaker Labs" , location: "220 King St W, Suite 200, Toronto, ON M5H 1K4", picture_url: "http://blog.bitmakerlabs.com/content/images/2015/09/blog-logo.png")
-@activity.owner = User.find(3)
-@activity.save
-@comment = @activity.comments.create(body: "These apps are amazing!")
-@comment.user = User.last
-@comment.save
-
 @activity = Activity.create(title: "Star Wars Movie Premiere: Always Tomorrow", description: "Jar Jar Binks is a Sith Lord.", end_time: DateTime.tomorrow, location_title: "Scotiabank Theatre", location: "259 Richmond St W, Toronto, ON M5V 3M6", picture_url: "http://pixel.nymag.com/imgs/daily/vulture/2015/10/18/18-star-wars-poster.w529.h529.jpg")
 @activity.owner = User.first
 @activity.save
@@ -31,13 +24,7 @@ User.create(email: "scmwalsh@gmail.com", password: "larry", password_confirmatio
 @activity.save
 
 @comment = @activity.comments.create(body: "Take me out to the ball game,
-Take me out with the crowd;
-Just buy me some peanuts and Cracker Jack,
-I don't care if I never get back.
-Let me root, root, root for the home team,
-If they don't win, it's a shame.
-For it's one, two, three strikes, you're out,
-At the old ball game.")
+Take me out with the crowd;")
 @comment.user = User.last
 @comment.save
 
@@ -45,5 +32,12 @@ At the old ball game.")
 @activity.owner = User.first
 @activity.save
 @acomment = @activity.comments.create(body: "Woah, I never thought this was going to be released!")
+@comment.user = User.last
+@comment.save
+
+@activity = Activity.create(title: "Bitmaker Demo-day NOW!", description: "Lets go Team Gibson.", end_time: DateTime.now, location_title: "Bitmaker Labs" , location: "220 King St W, Suite 200, Toronto, ON M5H 1K4", picture_url: "http://blog.bitmakerlabs.com/content/images/2015/09/blog-logo.png")
+@activity.owner = User.find(3)
+@activity.save
+@comment = @activity.comments.create(body: "These apps are amazing!")
 @comment.user = User.last
 @comment.save
