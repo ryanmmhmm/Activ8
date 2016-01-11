869f600631ab95b309b6c23ea2464eecdab0c3cc
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-10 23:24:15 -0500

    root route was changed, adjusting accordingly

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -72,9 +72,9 @@ class ActivitiesController < ApplicationController
     @activity = Activity.find(params[:id])
 
     if @activity.destroy
-      redirect_to :root, notice: 'Your Activity has be De-Activated'
+      redirect_to :index, notice: 'Your Activity has be De-Activated.'
     elsif @activity.errors.is_true?
-      flash.now[:alert] = 'Sorry, you can\'t deActivated that.'
+      flash.now[:alert] = 'Sorry, you can\'t deActivate that.'
       redirect_back_or_to (:activities)
     else
       logout(current_user)
